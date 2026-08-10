f1f456b32603ac6fdf250f430f5eb7ae1989bf9b
NachoRe06 <ire@mail.austral.edu.ar>
2026-08-10 10:28:39 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿const string NombreComercio = "KIOSCO EL RECREO";
-
-// Mostrar el nombre del comercio en la pantalla
 Console.WriteLine($"=== {NombreComercio} ===");
-
-// 2. Pedir el nombre del cajero
 Console.Write("Nombre del cajero: ");
 string nombreCajero = Console.ReadLine();
-
-// 3. Mostrar el saludo usando interpolación
 Console.WriteLine($"Bienvenido, {nombreCajero}. Caja abierta.");
+Console.Write("Nombre del producto: ");
+string nombreProducto = Console.ReadLine();
+Console.Write("Precio del producto: ");
+string entradaPrecio = Console.ReadLine();
+decimal precioProducto = decimal.Parse(entradaPrecio); 
+Console.WriteLine($"Producto cargado: {nombreProducto} - Precio: ${precioProducto}");
 
-// Esto evita que la ventanita negra se cierre de golpe al terminar
 Console.ReadLine();
