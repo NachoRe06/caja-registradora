fbbe0a3c21d1ab03e7c8a95036a43469037d0a71
NachoRe06 <ire@mail.austral.edu.ar>
2026-08-10 10:22:51 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿const string NombreComercio = "KIOSCO EL RECREO";
+
+// Mostrar el nombre del comercio en la pantalla
+Console.WriteLine($"=== {NombreComercio} ===");
+
+// 2. Pedir el nombre del cajero
+Console.Write("Nombre del cajero: ");
+string nombreCajero = Console.ReadLine();
+
+// 3. Mostrar el saludo usando interpolación
+Console.WriteLine($"Bienvenido, {nombreCajero}. Caja abierta.");
+
+// Esto evita que la ventanita negra se cierre de golpe al terminar
+Console.ReadLine();
